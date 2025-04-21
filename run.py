5079b8dbac0dd1b302411464d0969e5298e60914
kknb1982 <kirstin.barnett@gmail.com>
2025-04-21 16:12:24 +0100

    run code

---
+++ run.py
@@ -0,0 +1,13 @@
+
+
+#create virtual environment
+python -m venv venv
+
+# Run the virtual environment
+.venv\Scripts\activate.bat
+
+#Install the required packages
+pip install -r requirements.txt
+
+#Deactivate the virtual environment
+deactivate
