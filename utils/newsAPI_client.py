e4174154c376e8813b07f636eaa3d5749653dc59
kknb1982 <kirstin.barnett@gmail.com>
2025-05-03 18:17:50 +0100

    changed to news API

--- utils/newsAPI_client.py
+++ utils/newsAPI_client.py
@@ -5,31 +5,28 @@ from datetime import datetime, timedelta
 from data_handler import current_travel
 
 CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), '..', 'config.json')
-
 with open(CONFIG_FILE_PATH, 'r') as config_file:
     config = json.load(config_file)
-    
-WEBZIO_API_KEY = config.get('WEBZIO_API_KEY')
 
-if not WEBZIO_API_KEY:
-    raise ValueError("WEBZIO_API_KEY not found in config.json")
+NEWS_API_KEY = config.get('NEWS_API_KEY')
+if not NEWS_API_KEY:
+    raise ValueError("NEWS_API_KEY not found in config.json")
 
-WEBZIO_URL = "https://api.webz.io/newsApiLite"
+NEWS_API_URL = "https://newsapi.org/v2/everything"
 
 def fetch_news(location):
-    query = f"(+{location})"
-    
+    # Construct the query parameters
     parameters = {
-        'token': WEBZIO_API_KEY,
-        'q': query
+        'q': location,
+        'apiKey': NEWS_API_KEY
     }
     
     # Prepare the full URL with parameters
-    request = requests.Request('GET', WEBZIO_URL, params=parameters).prepare()
+    request = requests.Request('GET', NEWS_API_URL, params=parameters).prepare()
     print(f"Fetching news from URL: {request.url}")  # Print the full URL
     
     # Make the API request
-    response = requests.get(WEBZIO_URL, params=parameters)
+    response = requests.get(NEWS_API_URL, params=parameters)
     if response.status_code == 200:
         return response.json().get('articles', [])
     else:
@@ -37,5 +34,5 @@ def fetch_news(location):
         return None
 
 # Test the function
-news = fetch_news("Australia")
+news = fetch_news("Uppsala")
 print(news)
